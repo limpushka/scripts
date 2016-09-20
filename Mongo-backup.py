baacc32e4cc020feaa8c818d48a2b9cc9c766c4d
Denis Chernyakov <Denis Chernyakov>
2016-09-20 18:26:13 +1100

    123
    123

--- Mongo-backup.py
+++ Mongo-backup.py
@@ -31,10 +31,6 @@ def check_dir(path):
     if not os.path.exists(path):  
         os.makedirs(path) 
 
-def info():
-    logging.error("Please specify key arguments.--monthly - Option for Monthly Backup,--weekly - Option for Weekly Backup , -daily - Option for Daily Backup")
-    sys.exit()
-
 # Check disk space usage
 def get_disk_space():
     disk_space = psutil.disk_usage(storage_dir)
@@ -61,7 +57,6 @@ parser = argparse.ArgumentParser(description='Backup schedule options - Monthly,
 parser.add_argument('--monthly', '-m', action="store_true", help='Option for Monthly Backup')
 parser.add_argument('--weekly', '-w', action="store_true", help='Option for Weekly Backup')
 parser.add_argument('--daily', '-d', action="store_true", help='Option for Daily Backup')
-#parser.set_defaults(func=info)
                                      
 args = parser.parse_args()
 
@@ -83,7 +78,7 @@ elif args.daily:
     logging.info("Starting daily MongoDB backup")    
 else:
     logging.error("Please specify key arguments.--monthly - Option for Monthly Backup,--weekly - Option for Weekly Backup , -daily - Option for Daily Backup")
-    sys.exit()    
+    sys.exit("Please specify key arguments.--monthly - Option for Monthly Backup,--weekly - Option for Weekly Backup , -daily - Option for Daily Backup")    
 
 # Unlock and delete lock file.
 def un_lock():
