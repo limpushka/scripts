a4c0cfaff881e6080ac0afdf2879ebf5ece14489
Denis Chernyakov <Denis Chernyakov>
2016-09-20 17:57:47 +1100

    123
    123

--- Mongo-backup.py
+++ Mongo-backup.py
@@ -57,7 +57,7 @@ parser = argparse.ArgumentParser(description='Backup schedule options - Monthly,
 parser.add_argument('--monthly', '-m', action="store_true", help='Option for Monthly Backup')
 parser.add_argument('--weekly', '-w', action="store_true", help='Option for Weekly Backup')
 parser.add_argument('--daily', '-d', action="store_true", help='Option for Daily Backup')
-parser.add_argument(action="store_true", default='-h', help="don't print status messages to stdout")
+parser.set_defaults( action='-h', help="don't print status messages to stdout")
                                      
 args = parser.parse_args()
 
