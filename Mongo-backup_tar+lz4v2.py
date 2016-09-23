42fef3a3055959ab4d4659a4069c7e57856234c6
Denis Chernyakov <Denis Chernyakov>
2016-09-23 15:01:50 +1100

    123
    23

--- Mongo-backup_tar+lz4v2.py
+++ Mongo-backup_tar+lz4v2.py
@@ -193,8 +193,8 @@ class MongoDB:
         os.chdir(work_dir)
         tar_cmd = ['tar', 'cvf', '-', '%s' % self.db_name] 
         lz4_cmd = ['lz4', '>', '%s' % tar_name] 
-        tar = subprocess.Popen(tar_cmd.split(), stdout=subprocess.PIPE)
-        lz4 = subprocess.Popen(lz4_cmd.split(), stdin=tar.stdout, stdout=subprocess.PIPE)
+        tar = subprocess.Popen(tar_cmd, stdout=subprocess.PIPE)
+        lz4 = subprocess.Popen(lz4_cmd, stdin=tar.stdout, stdout=subprocess.PIPE)
         output = lz4.communicate()[0]        
         try:
             tar = subprocess.Popen(split(tar_cmd), stdout=subprocess.PIPE)
