045ef5c4a382f414f0da2ddd87603b68d2ff48e7
Denis Chernyakov <Denis Chernyakov>
2016-09-23 15:11:37 +1100

    123
    123

--- Mongo-backup_tar+lz4v2.py
+++ Mongo-backup_tar+lz4v2.py
@@ -15,7 +15,6 @@ import zipfile
 import psutil
 import zc.lockfile
 from shutil import copyfile, rmtree, copytree, move
-from shlex import split
 from pymongo import MongoClient
 
 
@@ -194,8 +193,8 @@ class MongoDB:
         os.chdir(work_dir)
         tar_cmd = ['tar', 'cvf', '-', '%s' % self.db_name] 
         lz4_cmd = ['lz4', '>', '%s' % tar_name] 
-        tar = subprocess.Popen(split(tar_cmd), stdout=subprocess.PIPE)
-        lz4 = subprocess.Popen(split(lz4_cmd), stdin=tar.stdout, stdout=subprocess.PIPE)
+        tar = subprocess.Popen(tar_cmd, stdout=subprocess.PIPE)
+        lz4 = subprocess.Popen(lz4_cmd, stdin=tar.stdout, stdout=subprocess.PIPE)
         output = lz4.communicate()[0]        
         try:
             tar = subprocess.Popen(split(tar_cmd), stdout=subprocess.PIPE)
